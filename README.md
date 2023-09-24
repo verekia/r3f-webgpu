ba791f0fe025a00561345303edb5394c4ebb061a
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2023-09-24 13:25:45 +0700

    chore: minimal repo for bug report

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# r3f-webgpu
+# Three + WebGPU issues
 
 ## How to use
 
@@ -37,41 +37,3 @@ TextureNode.js:52 Uncaught (in promise) TypeError: Cannot read properties of nul
 ### Minified code crashes
 
 Nodes module uses class names to register nodes (addNodeClass) which crashes in production: https://github.com/mrdoob/three.js/issues/26518
-
-## Drei helpers support
-
-- ✅ OrbitControls
-- ✅ useTexture
-- ✅ Instances
-
-## Performance
-
-It seems like WebGPU struggles to initialize the content of the scene more than WebGL. On a M1 Macbook Pro:
-
-- Rendering 100 boxes with WebGPU takes 40 seconds (freezes the tab).
-- Rendering 10.000 boxes with WebGL takes 2 seconds (runs at 5fps).
-
-## Notes
-
-### Set the clear color
-
-`renderer.setClearColor(0x000000, 0)`
-
-### Other way to set up the canvas (from Cody)
-
-```jsx
-<Canvas
-  onCreated={state => {
-    if ('gpu' in navigator) {
-      state.set({ frameloop: 'never' })
-      import('three/addons/renderers/webgpu/WebGPURenderer.js').then(
-        async ({ default: WebGPURenderer }) => {
-          const canvas = state.gl.domElement
-          const gl = await new WebGPURenderer(canvas).init()
-          state.set({ frameloop: 'always', gl })
-        }
-      )
-    }
-  }}
-/>
-```
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,8 @@ import { OrbitControls } from '@react-three/drei'
 import { Canvas, useThree } from '@react-three/fiber'
 
 import Box from '../components/Box'
-import UI from '../components/UI'
 
-const Scene = ({ extraBoxCount }: { extraBoxCount: number }) => {
+const Scene = () => {
   const { gl } = useThree()
 
   useEffect(() => {
@@ -24,16 +23,6 @@ const Scene = ({ extraBoxCount }: { extraBoxCount: number }) => {
       <pointLight intensity={10} position={[0, 0, 2]} castShadow />
       <Box position-x={-1.2} castShadow receiveShadow />
       <Box position-x={1.2} castShadow receiveShadow />
-      {Array.from({ length: extraBoxCount }).map((_, i) => (
-        <Box
-          key={i}
-          position-x={Math.random() * 10 - 5}
-          position-y={Math.random() * 6 - 3}
-          position-z={Math.random() * -10}
-          castShadow
-          receiveShadow
-        />
-      ))}
     </>
   )
 }
@@ -41,53 +30,36 @@ const Scene = ({ extraBoxCount }: { extraBoxCount: number }) => {
 let WebGPURenderer
 
 const IndexPage = () => {
-  const [isWebGPUAvailable, setIsWebGPUAvailable] = useState(false)
-  const [isWebGPU, setIsWebGPU] = useState(false)
   const [isReady, setIsReady] = useState(false)
-  const [extraBoxCount, setExtraBoxCount] = useState(0)
 
   useEffect(() => {
     const fn = async () => {
       // @ts-ignore
-      const capabilities = (await import('three/addons/capabilities/WebGPU.js')).default
-      // @ts-ignore
       WebGPURenderer = (await import('three/addons/renderers/webgpu/WebGPURenderer.js')).default
-      setIsWebGPUAvailable(capabilities.isAvailable())
-      setIsWebGPU(capabilities.isAvailable())
       setIsReady(true)
     }
     fn()
   }, [])
 
+  if (!isReady) {
+    return null
+  }
+
   return (
-    <>
-      <UI
-        isWebGPUAvailable={isWebGPUAvailable}
-        isWebGPU={isWebGPU}
-        setIsReady={setIsReady}
-        setIsWebGPU={setIsWebGPU}
-        extraBoxCount={extraBoxCount}
-        setExtraBoxCount={setExtraBoxCount}
-      />
-      {isReady && (
-        <Canvas
-          shadows
-          {...(isWebGPU && {
-            gl: canvas => {
-              if (!WebGPURenderer) {
-                throw Error('WebGPU renderer not loaded')
-              }
-              const r = new WebGPURenderer({ canvas })
-              r.setClearColor(0x000000, 0)
-              r.xr = { addEventListener: () => {} }
-              return r
-            },
-          })}
-        >
-          <Scene extraBoxCount={extraBoxCount} />
-        </Canvas>
-      )}
-    </>
+    <Canvas
+      shadows
+      gl={canvas => {
+        if (!WebGPURenderer) {
+          throw Error('WebGPU renderer not loaded')
+        }
+        const r = new WebGPURenderer({ canvas })
+        r.setClearColor(0x000000, 0)
+        r.xr = { addEventListener: () => {} }
+        return r
+      }}
+    >
+      <Scene />
+    </Canvas>
   )
 }
 
